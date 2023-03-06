a964692ba7847dd2bba5b5241828b807ade192dd
albinus <102544975+albios-dev@users.noreply.github.com>
2023-03-05 21:46:32 -0500

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,18 +1,18 @@
 <?php 
 // Website Name
-$webName = "AniKatsu";
+$webName = "AniZiti";
 // Website URL
 $webUrl = "//{$_SERVER['SERVER_NAME']}";  //for example $webUrl = "//{$_SERVER['SERVER_NAME']}:8888";
 // Logo
-$webLogo = "/files/images/logo.png";
+$webLogo = "/files/images/ziti.png";
 
 // Socials 
 // 1 . Donate 
-$donate = "https://r.honeygain.me/DEVEL77939";
+$donate = "#";
 // 2 . Telegram
-$telegram = "https://t.me/Anikatsu";
+$telegram = "#";
 // 3 . Discord 
-$discord = "https://discord.com/invite/FrWnWRrZmk";
+$discord = "#";
 // 4 Redit
 $redit = "#";
 // Twitter
